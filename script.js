6d07294da61ca522b7f9000e53eb6f0de8bd53a4
mrjoetwm <mrjoetech09@gmail.com>
2025-02-13 15:24:46 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-
- // Three.js 3D Animation
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setSize(window.innerWidth / 2, window.innerHeight / 2);
-// Add 3D text
-const loader = new THREE.FontLoader();
-loader.load('https://threejs.org/examples/fonts/helvetiker_regular.typeface.json', function (font) {
-  const textGeometry = new THREE.TextGeometry('Welcome', {
-    font: font,
-    size: 1,
-    height: 0.2,
-  });
-  const textMaterial = new THREE.MeshBasicMaterial({ color: 0xff6b6b });
-  const textMesh = new THREE.Mesh(textGeometry, textMaterial);
-  textMesh.position.set(-2, 0, 0);
-  scene.add(textMesh);
-});
-
-camera.position.z = 5;
-
-
-// Rest of the code remains the same
 // Sliding Background (only if element exists)
 const slidingBackground = document.querySelector('.sliding-background');
 if (slidingBackground) {
